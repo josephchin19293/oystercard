cc49f427c32c11e56c588ff13d3ff961ea9a4eea
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-04-18 17:18:26 +0100

    For real this time - chapter 4 complete.

--- README.md
+++
@@ -1,1 +0,0 @@
-Here's Emmett's test commit
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,13 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  describe "#initialize" do
+
+    it "should take a default balance of zero" do
+      expect(subject.balance).to eq 0
+    end
+
+  end
+
 end
