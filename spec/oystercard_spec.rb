e28dc88407054b06b77c836d6b1e72ae64db0afd
josephchin19293 <josephchin19293@gmail.com>
2016-04-18 17:24:46 +0100

    completes challenge 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
-
   describe "#initialize" do
-
-    it "should take a default balance of zero" do
+	it "should take a default balance of zero" do
       expect(subject.balance).to eq 0
     end
-
   end
+
+  describe "#top_up" do
+  	it 'adds value to the card balance' do
+  		subject.top_up(10)
+  		expect(subject.balance).to eq 10
+  	end
+  end
+
 
 end
